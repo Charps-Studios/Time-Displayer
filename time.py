a6d73f74877a057b939d35e80312d4b2cc8619cb
Charps-Studios <>
2021-02-27 11:38:52 +1300

    Got rid of the exit button.

--- time.py
+++ time.py
@@ -12,7 +12,7 @@ root = Tk()
 
 root.title("Time")
 root.overrideredirect(True)
-root.geometry("60x60+{w}+{h}".format(w = 0, h =root.winfo_screenheight() - 60))
+root.geometry("40x35+{w}+{h}".format(w = -1, h =root.winfo_screenheight() - 35))
 root.configure(bg='black')
 
 w, h = 50, 50
@@ -21,13 +21,9 @@ canvas = Canvas(root, width=0, height=0, highlightthickness=0)
 canvas.pack(fill='both')
 
 #Text for time
-w = Label(root, text="N/A", bg="black", fg="white")
+w = Button(root, text="N/A", command=root.destroy, bd=0, bg="black", fg="white")
 w['font'] = font.Font(family='Segoe UI Light')
 w.pack()
-
-# Button for closing 
-exit_button = Button(root, text="Exit", command=root.destroy, bd=0, bg="black", fg="white") 
-exit_button.pack(pady=0) 
 
 root.attributes('-topmost', True)
 root.attributes('-alpha', 0.75)
